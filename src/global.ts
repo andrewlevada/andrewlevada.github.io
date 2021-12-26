522862f56a5c04d4276bb30cbe97297c8006f637
AndrewLevada <levada.andrew@gmail.com>
2021-12-26 11:03:44 +0300

    Added global styles

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,6 @@
 import globalStyles from "~styles/global.scss";
 import globalPageStyles from "~src/pages/global-styles.scss";
+import layoutHelperStyles from "~src/pages/global-styles.scss";
 
-export const componentStyles = [globalStyles];
-export const pageStyles = [globalStyles, globalPageStyles];
+export const componentStyles = [globalStyles, layoutHelperStyles];
+export const pageStyles = [globalStyles, globalPageStyles, layoutHelperStyles];
