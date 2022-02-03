318d831aae2197f33a81c9d0b8f2b2277dc71251
AndrewLevada <levada.andrew@gmail.com>
2022-02-03 21:34:26 +0300

    Added contacts

--- src/components/contacts-section.ts
+++ src/components/contacts-section.ts
@@ -1,6 +1,7 @@
 import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { componentStyles } from "~src/global";
 import { defineComponent } from "~utils/components";
+import diamondImage from "../assets/diamond.svg";
 
 export default (): void => defineComponent("contacts-section", ContactsSection);
 export class ContactsSection extends LitElement {
@@ -8,6 +9,12 @@ export class ContactsSection extends LitElement {
         return html`
             <div class="wrapper flex row justify-between align-center full-width border">
                 <h4>Contact me here</h4>
+                <div class="flex row gap justify-end">
+                    ${["Telegram", "Github", "LinkedIn", "Email"].map((v, i) => html`
+                        ${i !== 0 ? html`<img src="${diamondImage}" alt="null">` : ""}
+                        <a class="header" href="${contactsMap[v.toLowerCase()]}" target="_blank">${v === "Email" ? "levada.andrew@gmail.com" : v}</a>
+                    `)}
+                </div>
             </div>
         `;
     }
@@ -22,3 +29,10 @@ export class ContactsSection extends LitElement {
         `];
     }
 }
+
+const contactsMap: Record<string, string> = {
+    telegram: "https://t.me/andrewlevada",
+    github: "https://github.com/andrewlevada",
+    linkedin: "https://www.linkedin.com/in/andrewlevada",
+    email: "mailto:levada.andrew@gmail.com",
+};
