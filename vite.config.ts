52720a45cc55e1f2aa72ac1ee0b59533e254f64a
AndrewLevada <levada.andrew@gmail.com>
2022-06-05 22:40:06 +0300

    feature(): added yandex metrika just to test

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,6 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      plugins: [
-        // litScss()
-          // litCss({
-          //   include: ["/**/*.scss", "/**/*.css"],
-          //   transform: (data, { filePath }) => compileString(data).css
-          // })
-      ]
-    }
-  },
   resolve: {
     alias: {
       "~services": "/src/services",
