e6ce2636e5aa69b3839c6e188fb652c00c7d473a
AndrewLevada <levada.andrew@gmail.com>
2022-10-19 16:47:36 +0300

    added URLPattern polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import("./pages/router").then(f => f.default())
+// @ts-ignore: Property 'UrlPattern' does not exist
+if (!globalThis.URLPattern)
+	import("urlpattern-polyfill").then(importRouter);
+else importRouter();
+
+function importRouter() {
+	import("./pages/router").then(f => f.default());
+}
 
 export {}
