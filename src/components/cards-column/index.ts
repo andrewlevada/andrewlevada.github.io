330665ed58112e46e63c603686c99eb89201ad32
AndrewLevada <levada.andrew@gmail.com>
2022-10-24 10:49:07 +0300

    :sparkles: added header gradien effects

--- src/components/cards-column/index.ts
+++ src/components/cards-column/index.ts
@@ -1,13 +1,22 @@
-/* eslint-disable indent */
-import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
+import { css, CSSResultGroup, html, LitElement, TemplateResult, unsafeCSS } from "lit";
 import { property, state } from "lit/decorators.js";
 import { componentStyles } from "~src/global";
 import { defineComponent, onLocaleChanged } from "~utils/components";
 import { Card, ContentSections, getCardsFor } from "~src/content-service";
 import { localized } from "@lit/localize";
+import scopedStyles from "./styles.scss";
+import jobsGradient from "~src/assets/graphics/gradients/jobs.png";
+import projectsGradient from "~src/assets/graphics/gradients/projects.png";
+import educationGradient from "~src/assets/graphics/gradients/education.png";
 
 import("~components/activity-card").then(f => f.default());
 import("~components/badge-card").then(f => f.default());
+
+const gradients = {
+	"jobs": jobsGradient,
+	"projects": projectsGradient,
+	"education": educationGradient,
+}
 
 export default (): void => defineComponent("cards-column", CardsColumn);
 @localized()
@@ -19,7 +28,9 @@ export class CardsColumn extends LitElement {
 	render(): TemplateResult {
 		return html`
 			<div class="flex col full-width top">
-				<div class="header flex pad-24"><h4>${this.header}</h4></div>
+				<div class="header flex pad-24 header-bg-${this.id}">
+					<h4>${this.header}</h4>
+				</div>
 				<div class="flex col full-width cards">
 					${this.cards.map(card => (card.link || card.paragraphs ? html`
 						<activity-card .content=${card} .isProject=${this.id === "projects"}></activity-card>
@@ -44,28 +55,11 @@ export class CardsColumn extends LitElement {
 	}
 
 	static get styles(): CSSResultGroup {
-		return [...componentStyles, css`
-			.top {
-				box-sizing: content-box;
-			}
-
-			@media screen and (max-width: 1023px) {
-				.top {
-					border: 2px var(--dark-color) solid;
+		return [...componentStyles, unsafeCSS(scopedStyles),
+			Object.entries(gradients).map(nameAndImage => css`
+				.header-bg-${unsafeCSS(nameAndImage[0])} {
+					background-image: url(${unsafeCSS(nameAndImage[1])});
 				}
-			}
-			
-			.header {
-				height: 62px;
-			}
-
-			.header > * {
-				align-self: center;
-			}
-
-			.cards > * {
-				border-top: solid 2px var(--dark-color);
-			}
-		`];
+			`)];
 	}
 }
--- src/pages/landing/parts/content.ts
+++ src/pages/landing/parts/content.ts
@@ -1,7 +1,8 @@
-import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
+import { CSSResultGroup, html, LitElement, TemplateResult, unsafeCSS } from "lit";
 import { componentStyles } from "~src/global";
 import { defineComponent } from "~utils/components";
 import { localized, msg } from "@lit/localize";
+import scopedStyles from "./content.scss";
 
 import("~components/cards-column").then(f => f.default());
 
@@ -16,71 +17,15 @@ export class ContentLandingPart extends LitElement {
 
 	render(): TemplateResult {
 		return html`
-			<div class="flex col full-width">
-				<div class="flex row full-width wrapper">
-					${this.columns.map((v, i) => html`
-						<cards-column class=${`col-${i + 1}`} .header=${v.name} .id=${v.id}></cards-column>
-					`)}
-				</div>
+			<div class="flex row full-width wrapper">
+				${this.columns.map((v, i) => html`
+					<cards-column class=${`col-${i + 1}`} .header=${v.name} .id=${v.id}></cards-column>
+				`)}
 			</div>
 		`;
 	}
 
 	static get styles(): CSSResultGroup {
-		return [...componentStyles, css`
-			.wrapper {
-				flex-wrap: wrap;
-			}
-
-			.wrapper > * {
-				flex-grow: 1;
-			}
-
-			.col-1 {
-				flex-basis: 36%;
-			}
-
-			.col-2 {
-				flex-basis: 42%;
-			}
-
-			.col-3 {
-				flex-basis: 22%;
-			}
-
-			@media screen and (min-width: 1024px) {
-				.wrapper {
-					border: solid 2px var(--dark-color);
-				}
-
-				.wrapper > *:not(:last-child) {
-					border-right: solid 2px var(--dark-color);
-				}
-			}
-
-			@media screen and (max-width: 1023px) {
-				.wrapper > * {
-					flex-basis: auto !important;
-				}
-
-				.wrapper > *:not(:last-child) {
-					margin-bottom: 32px;
-				}
-			}
-
-			@media screen and (max-width: 1439px) {
-				.col-1 {
-					flex-basis: 35%;
-				}
-
-				.col-2 {
-					flex-basis: 35%;
-				}
-
-				.col-3 {
-					flex-basis: 30%;
-				}
-			}
-		`];
+		return [...componentStyles, unsafeCSS(scopedStyles)];
 	}
 }
