fca7f497e30a7ac049f3bed665cb56828912779f
AndrewLevada <levada.andrew@gmail.com>
2022-10-25 12:01:41 +0300

    :sparkles: added gradient title effects

--- src/pages/landing/parts/title.ts
+++ src/pages/landing/parts/title.ts
@@ -1,7 +1,10 @@
-import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
+import { css, CSSResultGroup, html, LitElement, TemplateResult, unsafeCSS } from "lit";
 import { componentStyles } from "~src/global";
 import { defineComponent } from "~utils/components";
 import { localized, msg } from "@lit/localize";
+import shortTitleEffect from "~src/assets/graphics/gradients/title-short.png";
+import longTitleEffect from "~src/assets/graphics/gradients/title-long.png";
+import scopedStyles from "./title.scss";
 
 export default (): void => defineComponent("title-landing-part", TitleLandingPart);
 @localized()
@@ -9,22 +12,22 @@ export class TitleLandingPart extends LitElement {
 	render(): TemplateResult {
 		return html`
 			<div class="flex col justify-center align-center full-width full-height">
-				<div class="flex row justify-between full-width">
-					<h1 id="name-header">${msg("Hey! I’m Andrew,")}</h1>
-					<h1>${msg("Aspiring UX Engineer with management, coding and design skills.")}</h1>
+				<div class="texts flex row justify-between full-width">
+					<h1 id="title-short">${msg("Hey! I’m Andrew,")}</h1>
+					<h1 id="title-long">${msg("Aspiring UX Engineer with management, coding and design skills.")}</h1>
 				</div>
 			</div>
 		`;
 	}
 
 	static get styles(): CSSResultGroup {
-		return [...componentStyles, css`
-			h1 {
-				width: min-content;
+		return [...componentStyles, unsafeCSS(scopedStyles), css`
+			#title-short:before {
+				background-image: url(${unsafeCSS(shortTitleEffect)});
 			}
 			
-			#name-header {
-				transform: translateY(-82px);
+			#title-long:before {
+				background-image: url(${unsafeCSS(longTitleEffect)});
 			}
 		`];
 	}
