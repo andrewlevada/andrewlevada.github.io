53ab9bdf4df088667f741cd9f90a052e769e4136
AndrewLevada <levada.andrew@gmail.com>
2022-10-27 15:03:50 +0300

    :bug: fixed print button localization update

--- src/components/large-button/cases/print-button.ts
+++ src/components/large-button/cases/print-button.ts
@@ -2,15 +2,20 @@ import { defineComponent } from "~utils/components";
 import { localized, msg } from "@lit/localize";
 import { getLocale } from "~src/global";
 import { LargeButton } from "../index";
+import { PropertyValues } from "lit";
 
 export default (): void => defineComponent("print-button", PrintButton);
 @localized()
 export class PrintButton extends LargeButton {
 	constructor() {
 		super();
+		this.icon = "print";
+	}
+
+	protected willUpdate(_changedProperties: PropertyValues) {
+		super.willUpdate(_changedProperties);
 		this.text = msg("Get PDF");
 		this.href = `/static/print_${getLocale()}.pdf`;
 		this.download = msg("CV Andrew Levada");
-		this.icon = "print";
 	}
 }
